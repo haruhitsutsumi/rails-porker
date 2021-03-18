53f5cd5dc71bd05b871ebc2e74851d8bd794d522
htsutsumi <htsutsumi@netprotections.co.jp>
2021-03-18 15:09:04 +0000

    イニシャライザの利用とメソッドのprivate化。カードチェックの短縮

--- app/controllers/hands_controller.rb
+++ app/controllers/hands_controller.rb
@@ -2,19 +2,18 @@ class HandsController < ApplicationController
 
   def top
     @input = nil
-    @hand = Hand.new
+    @hand = Hand.new("")
+
   end
 
   def judge
-    @hand = Hand.new(
-      cards: params[:hands],
-      hands: params[:hands].split(" "))
+    @hand = Hand.new(params[:hands])
     if @hand.valid? == false
       @input = params[:hands]
       render("hands/top")
     else
     #格納した数字を元に各メソッドを呼び出し
-      @hand.finalj = @hand.finaljudge
+      @hand.finalj = @hand.judge
       @input = params[:hands]
       render("hands/top")
     end
--- app/models/hand.rb
+++ app/models/hand.rb
@@ -5,12 +5,12 @@ attr_accessor :hands, :cards, :finalj, :error_message
 FORMATCHECK = /\b[^ ]+\b[ ]{1}\b[^ ]+\b[ ]{1}\b[^ ]+\b[ ]{1}\b[^ ]+\b[ ]{1}\b[^ ]+\b$/
 LETTERCHECK = /\b[SCHD]([1-9]|1[0-3])\b/
 
-=begin
+
 def initialize(cards)
   self.cards = cards
   self.hands = self.cards.split(" ")
 end
-=end
+
 
   def valid?
     #全体の形式チェック
@@ -18,15 +18,8 @@ end
       self.error_message ="5つのカード指定文字を半角スペース区切りで入力してください。（例："+"S1 H3 D9 C13 S11"+"）"
       @input = self.cards
       return false
-
     #文字形式チェック
-    elsif
-      self.hands[0].match(LETTERCHECK) == nil ||
-      self.hands[1].match(LETTERCHECK) == nil ||
-      self.hands[2].match(LETTERCHECK) == nil||
-      self.hands[3].match(LETTERCHECK) == nil||
-      self.hands[4].match(LETTERCHECK) == nil
-
+    elsif self.hands.grep(LETTERCHECK).count != 5
       self.error_message=""
       self.hands.each_with_index do|card,i|
         if card.match(LETTERCHECK) == nil
@@ -35,7 +28,6 @@ end
       end
       self.error_message +="半角英字大文字のスート（S,H,D,C）と数字（1〜13）の組み合わせでカードを指定してください。"
       return false
-
     #重複チェック
     elsif self.hands.uniq.length != 5
       self.error_message ="カードが重複してます。"
@@ -44,24 +36,24 @@ end
   end
 
   #flash,pair,straightの判定に基づき役名の判定を行う
-  def finaljudge
-    if self.flash? == true && self.straight? == true
+  def judge
+    if flash?(self.hands) == true && straight?(self.hands) == true
       return "ストレート・フラッシュ"
-    elsif self.flash? == true && self.straight? == false
+    elsif flash?(self.hands) == true && straight?(self.hands) == false
       return "フラッシュ"
-    elsif self.flash? == false && self.straight? == true
+    elsif flash?(self.hands) == false && straight?(self.hands) == true
       return "ストレート"
-    elsif self.flash? == false && self.straight? == false && self.pairjudge == "highcard"
+    elsif flash?(self.hands) == false && straight?(self.hands) == false && pairjudge(self.hands) == "highcard"
       return "ハイカード"
-    elsif self.pairjudge == "fourcard"
+    elsif pairjudge(self.hands) == "fourcard"
       return "フォーカード"
-    elsif self.pairjudge == "fullhouse"
+    elsif pairjudge(self.hands) == "fullhouse"
       return "フルハウス"
-    elsif self.pairjudge == "threecard"
+    elsif pairjudge(self.hands)  == "threecard"
       return "スリーカード"
-    elsif self.pairjudge == "twopair"
+    elsif pairjudge(self.hands) == "twopair"
       return "ツーペア"
-    elsif self.pairjudge == "onepair"
+    elsif pairjudge(self.hands) == "onepair"
       return "ワンペア"
     else
       return "プータロー"
@@ -69,12 +61,13 @@ end
   end
 
   #flashの判定を行う
-  def flash?
-    if [self.hands[0][/s|d|c|h/],
-      self.hands[1][/s|d|c|h/],
-      self.hands[2][/s|d|c|h/],
-      self.hands[3][/s|d|c|h/],
-      self.hands[4][/s|d|c|h/]].uniq.length == 1
+  def flash?(hands)
+    if [hands[0][/S|D|C|H/],
+      hands[1][/S|D|C|H/],
+      hands[2][/S|D|C|H/],
+      hands[3][/S|D|C|H/],
+      hands[4][/S|D|C|H/]].uniq.length == 1
+
       return true
     else
       return false
@@ -82,35 +75,38 @@ end
   end
 
   #straightの判定を行う
-  def straight?
+  def straight?(hands)
     handsarraynum = [
-      self.hands[0][/([1-9]|1[0-3])\b/].to_i,
-      self.hands[1][/([1-9]|1[0-3])\b/].to_i,
-      self.hands[2][/([1-9]|1[0-3])\b/].to_i,
-      self.hands[3][/([1-9]|1[0-3])\b/].to_i,
-      self.hands[4][/([1-9]|1[0-3])\b/].to_i]
+      hands[0][/([1-9]|1[0-3])\b/].to_i,
+      hands[1][/([1-9]|1[0-3])\b/].to_i,
+      hands[2][/([1-9]|1[0-3])\b/].to_i,
+      hands[3][/([1-9]|1[0-3])\b/].to_i,
+      hands[4][/([1-9]|1[0-3])\b/].to_i]
 
     if
-       (handsarraynum.sort![4] - handsarraynum.sort![3] == 1||9) &&
-       (handsarraynum.sort![3] - handsarraynum.sort![2] == 1||9) &&
-       (handsarraynum.sort![2] - handsarraynum.sort![1] == 1||9) &&
-       (handsarraynum.sort![1] - handsarraynum.sort![0] == 1||9)
+       (handsarraynum.sort![4] - handsarraynum.sort![3] == 1||
+       handsarraynum.sort![4] - handsarraynum.sort![3] == 9) &&
+       (handsarraynum.sort![3] - handsarraynum.sort![2] == 1||
+       handsarraynum.sort![3] - handsarraynum.sort![2] == 9) &&
+       (handsarraynum.sort![2] - handsarraynum.sort![1] == 1||
+       handsarraynum.sort![2] - handsarraynum.sort![1] == 9) &&
+       (handsarraynum.sort![1] - handsarraynum.sort![0] == 1||
+       handsarraynum.sort![1] - handsarraynum.sort![0] == 9)
       return true
     else
       return false
     end
-    
   end
 
   #pairの判定を行う
-  def pairjudge
+  def pairjudge(hands)
     #インスタンスで持ってる各カードから数字を抽出し、配列を作成
     handsarraynum = [
-      self.hands[0][/([1-9]|1[0-3])\b/],
-      self.hands[1][/([1-9]|1[0-3])\b/],
-      self.hands[2][/([1-9]|1[0-3])\b/],
-      self.hands[3][/([1-9]|1[0-3])\b/],
-      self.hands[4][/([1-9]|1[0-3])\b/]]
+      hands[0][/([1-9]|1[0-3])\b/],
+      hands[1][/([1-9]|1[0-3])\b/],
+      hands[2][/([1-9]|1[0-3])\b/],
+      hands[3][/([1-9]|1[0-3])\b/],
+      hands[4][/([1-9]|1[0-3])\b/]]
 
     #数字だけの配列を、数字ごとにグルーピングしたハッシュを作り、ハッシュのキーをカウントした配列を作ってる
     handsarraynumcount = handsarraynum.group_by{|han| han}.map{|k,v| v.count}
@@ -129,7 +125,5 @@ end
       return "highcard"
     end
   end
-
-#  private :flash?, :pairjudge, :straight?
-
+  private :flash?, :pairjudge, :straight?
 end
