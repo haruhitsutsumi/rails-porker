2c10d2251058ba5cf20fccc1f38051fa24c55ede
htsutsumi <htsutsumi@netprotections.co.jp>
2021-03-23 13:00:14 +0000

    ifを一節で記載

--- app/services/poker_hand.rb
+++ app/services/poker_hand.rb
@@ -19,9 +19,7 @@ LETTERCHECK = /\b[SCHD]([1-9]|1[0-3])\b/
     elsif self.cards.grep(LETTERCHECK).count != 5
       self.error_message=""
       self.cards.each_with_index do|card,i|
-        if card.match(LETTERCHECK) == nil
-          self.error_message += "#{i+1}番目のカード指定文字が不正です。（#{self.cards[i]}）\r"
-        end
+        self.error_message += "#{i+1}番目のカード指定文字が不正です。（#{self.cards[i]}）\r" if card.match(LETTERCHECK) == nil
       end
       self.error_message +="半角英字大文字のスート（S,H,D,C）と数字（1〜13）の組み合わせでカードを指定してください。"
       return false
